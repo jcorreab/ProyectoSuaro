fd462533e040cb9050414903c43fa252a498462a
Xavi2620 <aherrera@birobid.com>
2023-03-06 02:06:27 -0500

    cambios

--- src/sections/app/soporte/pages/SoportePage.js
+++ src/sections/app/soporte/pages/SoportePage.js
@@ -16,38 +16,96 @@ import { BotonGrabar, BotonNuevo } from '../../../../components/app/Botones';
 
 function SoportePage() {
   return (
-    <Grid container spacing={1}>
-      <Grid item container md={6} sm={12} xs={12}>
-        <CajaGenerica
-          //   inputRef={estudianteRef}
-          estadoInicial={{
-            codigoAlternativo: 'estudiante.codigo',
-            nombre: 'estudiante.nombre',
-          }}
-          tituloTexto={{ nombre: 'Cliente', descripcion: 'Nombre' }}
-          tituloModal="Estudiantes"
-          retornarDatos={(e) => {
-            // cambiarEstudiante(e);
-          }}
-          datos={[]}
-        />
+    <Transicion>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant="h4">Soporte</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider>
+            <Chip label="Formulario" />
+          </Divider>
+        </Grid>
+        <Grid item container md={6} sm={12} xs={12}>
+          <CajaGenerica
+            //   inputRef={estudianteRef}
+            estadoInicial={{
+              codigoAlternativo: 'estudiante.codigo',
+              nombre: 'estudiante.nombre',
+            }}
+            tituloTexto={{ nombre: 'Cliente', descripcion: 'Nombre' }}
+            tituloModal="Estudiantes"
+            retornarDatos={(e) => {
+              // cambiarEstudiante(e);
+            }}
+            datos={[]}
+          />
+        </Grid>
+        <Grid item container md={6} sm={12} xs={12}>
+          <CajaGenerica
+            //   inputRef={estudianteRef}
+            estadoInicial={{
+              codigoAlternativo: 'estudiante.codigo',
+              nombre: 'estudiante.nombre',
+            }}
+            tituloTexto={{ nombre: 'Tecnico', descripcion: 'Nombre' }}
+            tituloModal="Estudiantes"
+            retornarDatos={(e) => {
+              // cambiarEstudiante(e);
+            }}
+            datos={[]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={2} md={2}>
+          <BotonGrabar
+            propiedades={{
+              onClick: () => {},
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={2} md={2}>
+          <BotonNuevo
+            propiedades={{
+              onClick: () => {},
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Divider>
+            <Chip label="Lista de Soportes" />
+          </Divider>
+        </Grid>
+        <Grid item xs={12} sm={7} md={5}>
+          <TextField
+            fullWidth
+            size="small"
+            label="Buscar"
+            variant="filled"
+            // value={buscar}
+            // onChange={(e) => cambiarBusqueda(e)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={estiloTabla}>
+            <div style={{ height: '30vh', width: '100%' }}>
+              <DataGrid
+                density="compact"
+                rowHeight={28}
+                localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+                sx={estiloDatagrid}
+                components={{
+                  NoRowsOverlay: IconoDataGrid,
+                }}
+                // onRowClick={(e) => obtenerRegistro(e)}
+                columns={[]}
+                rows={[]}
+                getRowId={(rows) => rows.codigo}
+              />
+            </div>
+          </Box>
+        </Grid>
       </Grid>
-      <Grid item container md={6} sm={12} xs={12}>
-        <CajaGenerica
-          //   inputRef={estudianteRef}
-          estadoInicial={{
-            codigoAlternativo: 'estudiante.codigo',
-            nombre: 'estudiante.nombre',
-          }}
-          tituloTexto={{ nombre: 'Tecnico', descripcion: 'Nombre' }}
-          tituloModal="Estudiantes"
-          retornarDatos={(e) => {
-            // cambiarEstudiante(e);
-          }}
-          datos={[]}
-        />
-      </Grid>
-    </Grid>
+    </Transicion>
   );
 }
 
